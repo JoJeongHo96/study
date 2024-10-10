7fddbfe68374da65a258a4225732910ce322c4f8
정호찡 <@121.128.163.46>
2024-10-10 22:57:14 +0900

    Enum.valueOf는 Enum의 소속된 상수와 완벽 일치해야하며 다르면 java.lang.IllegalArgumentException

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,8 @@ public class DemoApplication {
 		
 		// Q: Enum의 valueOf는 값이 없으면 뭘 반환 할까
 		System.out.println(jjhEnum.valueOf("one")); //일치
-		System.out.println(jjhEnum.valueOf("Two")); // 대문자도 구분해? >> 컴파일 에러
-		System.out.println(jjhEnum.valueOf("Four")); // 마찬가지로 컴파일 에러.
+		System.out.println(jjhEnum.valueOf("Two")); // 대문자도 구분해? >> 런타임 에러
+		System.out.println(jjhEnum.valueOf("Four")); // 마찬가지로 런타임 에러.
 		
 	}
 

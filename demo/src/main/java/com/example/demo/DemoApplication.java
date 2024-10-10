18fc693b3c14f1024b8d909c6f80924eb1efbea2
정호찡 <@121.128.163.46>
2024-10-10 22:56:33 +0900

    Enum.valueOf는 Enum의 소속된 상수와 완벽 일치해야하며 다르면 java.lang.IllegalArgumentException

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,16 +4,12 @@ package com.example.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) throws Exception {
-//		SpringApplication.run(DemoApplication.class, args);
 		
-		Thread t = Thread.currentThread();
-		System.out.println(t); //Thread[main,5,main]
-								// Thread[스레드이름,우선순위,스레드그룹이름]
-	
-		System.out.println(t.getName());
-		System.out.println(t.getPriority());
-		System.out.println(t.getThreadGroup());
-		System.out.println(t.getThreadGroup().getName());
+		// Q: Enum의 valueOf는 값이 없으면 뭘 반환 할까
+		System.out.println(jjhEnum.valueOf("one")); //일치
+		System.out.println(jjhEnum.valueOf("Two")); // 대문자도 구분해? >> 컴파일 에러
+		System.out.println(jjhEnum.valueOf("Four")); // 마찬가지로 컴파일 에러.
+		
 	}
 
 }
